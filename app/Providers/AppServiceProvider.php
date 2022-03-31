6a5f4c30ab77f9651999b84f6334f25db61d075b
tholscoa <akinnuoyetolulope@gmail.com>
2022-03-31 16:53:02 +0100

    defined Super Admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,39 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [];
+
+    /**
+     * Register the application's policies.
+     *
+     * @return void
+     */
+    public function registerPolicies()
+    {
+        foreach ($this->policies() as $key => $value) {
+            Gate::policy($key, $value);
+        }
+    }
+
+    /**
+     * Get the policies defined on the provider.
+     *
+     * @return array
+     */
+    public function policies()
+    {
+        return $this->policies;
+    }
+    
     /**
      * Register any application services.
      *
@@ -16,6 +46,14 @@ class AppServiceProvider extends ServiceProvider
         //
         $this->app->singleton(\Illuminate\Contracts\Routing\ResponseFactory::class, function () {
             return new \Laravel\Lumen\Http\ResponseFactory();
+        });
+
+        $this->registerPolicies();
+
+        // Implicitly grant "Super Admin" role all permissions
+        // This works in the app by using gate-related functions like auth()->user->can() and @can()
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
         });
     }
 }
